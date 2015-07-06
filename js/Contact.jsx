3687f3861137decb22bf6759410f3543b3a68d7c
Levi Tan Ong <levi@byimplication.com>
2015-07-07 01:49:20 +0800

    refinements

--- js/Contact.jsx
+++ js/Contact.jsx
@@ -4,9 +4,9 @@ export default class Contact extends React.Component {
 	render() {
 		return (
 			<section id="Contact">
-				<h1>Drop us a line</h1>
-				<h2>example@email.com</h2>
-				<h1>Visit us</h1>
+				<h2>Drop us a line</h2>
+				<h3>example@email.com</h3>
+				<h2>Visit us</h2>
 				<div>
 					map
 				</div>
