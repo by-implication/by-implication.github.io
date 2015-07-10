fa3a79ebed59d330786da3866363c8557f6befb1
Levi Tan Ong <levi@byimplication.com>
2015-07-10 21:23:57 +0800

    image gallery

--- js/CaseStudy.jsx
+++ js/CaseStudy.jsx
@@ -6,7 +6,20 @@ import portfolioData from "../data/portfolio-data";
 import {TextButton} from "./Buttons";
 
 export default class CaseStudy extends React.Component {
-	componentDidMount() {
+	constructor(props) {
+		super(props);
+		this.state = {
+			images: []
+		}
+	}
+	componentDidMount() {	
+		const context = require.context("../data/portfolio-casestudy/", true, /\.*/);
+		const images = context
+			.keys()
+			.filter(x => RegExp(this.props.params.id + "/images").test(x))
+			.map(x => context(x));
+		this.setState({images: images});
+		// console.log(this.state.images);
 	}
 	render() {
 		const data = portfolioData[this.props.params.id];
@@ -36,11 +49,16 @@ export default class CaseStudy extends React.Component {
 							</TextButton>
 						</dl>
 					</aside>
-					{ 
-						data.caseStudy
-							? <article dangerouslySetInnerHTML={ {__html: data.caseStudy } }/> 
-							: defaultWriteup
-					}
+					<article>
+						{ 
+							data.caseStudy
+								? <section dangerouslySetInnerHTML={ {__html: data.caseStudy } }/> 
+								: defaultWriteup
+						}				
+						<section className="image-gallery">
+							{ this.state && this.state.images.map((x, i) => <img key={"image" + i} src={x} />) }
+						</section>
+					</article>
 				</div>
 			</div>
 		);
