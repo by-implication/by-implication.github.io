a70561c8dd9c5718a1a981a538a28802b76a7d12
Levi Tan Ong <levi@byimplication.com>
2015-07-24 15:01:22 +0800

    adds fastclick

--- js/Wrapper.jsx
+++ js/Wrapper.jsx
@@ -10,6 +10,19 @@ const ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
 class Wrapper extends React.Component {
 	constructor(props) {
 		super(props);
+		// if (typeof document !== "undefined") {
+		// 	const fastclick = require("fastclick");
+		// 	console.log(document.body);
+		// 	fastclick(document.body);
+		// }
+	}
+	componentDidMount() {
+		if (typeof window !== "undefined") {
+			window.addEventListener('load', () => {
+				const fastclick = require("fastclick");
+			  fastclick.attach(document.body);
+			});
+		}
 	}
 	meta(routeName) {
 		switch(routeName) {
