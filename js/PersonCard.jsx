6d8da195e633b3d8513356282c5e7ddbe0cb70e3
Philip Cheang <phi@byimplication.com>
2015-07-27 12:53:43 +0800

    make whole PersonCard link if URL available

--- js/PersonCard.jsx
+++ js/PersonCard.jsx
@@ -1,27 +1,40 @@
 import PersonCardStyle from "../css/PersonCard.css";
 import React from 'react';
 
+
 export default class PersonCard extends React.Component {
 	constructor(props) {
 		super(props);
 	}
+
 	render() {
-		return (
-			<div className="PersonCard">
-				<img className="portrait" src={ this.props.imageSrc } alt={ "A nice photo of " + this.props.nickname + "." } title={ "A nice photo of " + this.props.nickname + "." } />
-				<h4>
-					{
-						this.props.link
-							? <a className="person-link" target="_blank" href={ this.props.link }>{ this.props.name }</a>
-							: this.props.name
-					}
-				</h4>
-				<h5>{ this.props.roles }</h5>
-			</div>
-		);
+		var personContent;
+		if (this.props.link) {
+			return (
+				<a href={ this.props.link } className="PersonCard">
+					<img className="portrait" src={ this.props.imageSrc } alt={ "A nice photo of " + this.props.nickname + "." } title={ "A nice photo of " + this.props.nickname + "." } />
+					<h4>
+						<span className="person-link">
+						{ this.props.name }
+						</span>
+					</h4>
+					<h5>{ this.props.roles }</h5>
+				</a>
+			);
+		} else {
+			return (
+				<div className="PersonCard">
+					<img className="portrait" src={ this.props.imageSrc } alt={ "A nice photo of " + this.props.nickname + "." } title={ "A nice photo of " + this.props.nickname + "." } />
+					<h4>
+						{ this.props.name	}
+					</h4>
+					<h5>{ this.props.roles }</h5>
+				</div>
+			);
+		}
 	}
 }
 
 PersonCard.propTypes = {
 	name: React.PropTypes.string
-}
+};
