fc5476c102a79dcecb9fd5489020fd718f4e7d1d
Philip Cheang <phi@byimplication.com>
2015-07-27 14:29:09 +0800

    make PersonCard more DRY
    thanks Leev <3

--- js/PersonCard.jsx
+++ js/PersonCard.jsx
@@ -1,5 +1,6 @@
 import PersonCardStyle from "../css/PersonCard.css";
 import React from 'react';
+import classnames from "classnames";
 
 
 export default class PersonCard extends React.Component {
@@ -9,29 +10,27 @@ export default class PersonCard extends React.Component {
 
 	render() {
 		var personContent;
-		if (this.props.link) {
-			return (
-				<a href={ this.props.link } className="PersonCard">
-					<img className="portrait" src={ this.props.imageSrc } alt={ "A nice photo of " + this.props.nickname + "." } title={ "A nice photo of " + this.props.nickname + "." } />
-					<h4>
-						<span className="person-link">
-						{ this.props.name }
-						</span>
-					</h4>
-					<h5>{ this.props.roles }</h5>
-				</a>
-			);
-		} else {
-			return (
-				<div className="PersonCard">
-					<img className="portrait" src={ this.props.imageSrc } alt={ "A nice photo of " + this.props.nickname + "." } title={ "A nice photo of " + this.props.nickname + "." } />
-					<h4>
-						{ this.props.name	}
-					</h4>
-					<h5>{ this.props.roles }</h5>
-				</div>
-			);
-		}
+		const cardContent = (
+			<div className="cardContent">
+				<img className="portrait" src={ this.props.imageSrc } alt={ "A nice photo of " + this.props.nickname + "." } title={ "A nice photo of " + this.props.nickname + "." } />
+				<h4>
+					<span className={ classnames({"person-link": this.props.link}) }>
+					{ this.props.name }
+					</span>
+				</h4>
+				<h5>{ this.props.roles }</h5>
+			</div>
+		);
+		let cardProps = {className: "PersonCard"};
+		if (this.props.link) { cardProps.href = this.props.link };
+		const Card = React.createElement(
+			this.props.link ? "a" : "div",
+			cardProps,
+			[
+				cardContent
+			]
+		);
+		return Card;
 	}
 }
 
